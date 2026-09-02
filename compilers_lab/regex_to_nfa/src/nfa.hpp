8d43d999d424f5ac07918eaadba201e32b02ad55
aricc24 <aricc24@ciencias.unam.mx>
2026-09-01 19:40:23 -0600

    data structre for NFA

--- compilers_lab/regex_to_nfa/src/nfa.hpp
+++ compilers_lab/regex_to_nfa/src/nfa.hpp
@@ -3,12 +3,31 @@
 
 #include <string>
 
+#include <string>
+#include <vector>
 #include "regex.hpp"
+
+constexpr char EPSILON = '\0';
+
+struct Transition
+{
+    int from;
+    int to;
+    char symbol; //might be epsilon
+};
+
 
 struct Nfa
 {
-    int placeholder;
+    int num_states = 0; 
+    std::vector<Transition> transition; 
+    int start_sate = -1; 
+    int accept_state = -1; 
+
+    int add_state(); 
 };
+
+
 
 Nfa regex_to_nfa(const Regex &r);
 bool match_nfa(const Nfa &n, const std::string &input);
